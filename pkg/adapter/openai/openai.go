03d392305c12e4e9326fe13ffd1f568b35971630
minhlq <minhlq0696@gmail.com>
2023-06-15 17:17:11 +0700

    feat: sum enhancement

--- pkg/adapter/openai/openai.go
+++ pkg/adapter/openai/openai.go
@@ -26,8 +26,12 @@ func (o *OpenAI) SummarizeArticle(url string) (string, error) {
 			Model: openai.GPT3Dot5Turbo,
 			Messages: []openai.ChatCompletionMessage{
 				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: fmt.Sprintf("Summarize this artile: %s. The summary should be 125 words max.", url),
+					Role: openai.ChatMessageRoleUser,
+					Content: fmt.Sprintf(`You are able to accurately read news, books, and summaries.
+					 you should be able to comprehend the content and provide a detailed understanding of the material.
+					 Additionally, you should be able to identify key points and summarize the material in an organized manner.
+					 You should also be able to recognize any errors or inconsistencies in the material and provide feedback on how to improve it.
+					Summarize this artile: %s. The summary should be 200 words max.`, url),
 				},
 			},
 		},
