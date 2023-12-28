80d2a32d3c667a98bbbf2a5f2808c1d80003fc4f
Nam Nguyen <namnhce@gmail.com>
2023-12-28 12:36:35 +0700

    chore: add more info to the profile command (#32)

--- pkg/discord/view/profile/interface.go
+++ pkg/discord/view/profile/interface.go
@@ -4,5 +4,7 @@ import "github.com/dwarvesf/fortress-discord/pkg/model"
 
 type Viewer interface {
 	Help(message *model.DiscordMessage) error
-	List(original *model.DiscordMessage, employee []model.Employee) error
+	Render(original *model.DiscordMessage, employees []model.Employee) error
+	List(original *model.DiscordMessage, employees []model.Employee) error
+	Details(original *model.DiscordMessage, employee *model.Employee) error
 }
