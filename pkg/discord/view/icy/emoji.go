662ecd60820943e5c7e66139c9fc2ce95fe4dafa
Bien Vo <vhbien000@gmail.com>
2024-01-05 16:48:13 +0700

    fix: wrong animated emoji format

--- pkg/discord/view/icy/emoji.go
+++ pkg/discord/view/icy/emoji.go
@@ -8,7 +8,7 @@ const (
 	discordEmojiAnxinICY         = "<:anxinicy:1014483263705862174>"
 	discordEmojiAnxin            = "<:anxin:973799916147179550>"
 	discordEmojiPepeBusiness     = "<:pepebusiness:885513213687504936>"
-	discordEmojiPepeWhale        = "<:pepewhale:902558994437144646>"
+	discordEmojiPepeWhale        = "<a:pepewhale:902558994437144646>"
 	discordEmojiIcyToken         = "<:ICY:1049620715374133288>"
 	discordEmojiPepeStonk        = "<:pepestonk:850050324135673937>"
 	discordEmojiPepeAhegao       = "<:pepeahegao:824711657837297705>"
