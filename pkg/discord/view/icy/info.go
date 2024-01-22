2863f17d0616f64641cc26e3cb11f3bea0df76f4
Bien Vo <vhbien000@gmail.com>
2024-01-22 20:02:14 +0700

    fix: refactor icy discovery view

--- pkg/discord/view/icy/info.go
+++ pkg/discord/view/icy/info.go
@@ -6,29 +6,104 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+
 	"github.com/dwarvesf/fortress-discord/pkg/discord/view/base"
 	"github.com/dwarvesf/fortress-discord/pkg/model"
 	"github.com/dwarvesf/fortress-discord/pkg/utils/cryptoutils"
 )
 
 func (e *Icy) PersonalInfo(original *model.DiscordMessage, accounting *model.IcyAccounting, totalEarned *model.ICYTotalEarned, earnedTxns []*model.ICYEarnedTransaction) error {
-	if original == nil || accounting == nil || accounting.ICY == nil || accounting.USDT == nil || totalEarned == nil || earnedTxns == nil {
+	// Do nothing when there is no message to reply to
+	if original == nil {
 		return nil
 	}
 
-	var formattedContractFund string = "NaN"
-	if fund := cryptoutils.StringBigIntToStringCurrency(accounting.ContractFundInUSDT, accounting.USDT.Decimals); fund != "" {
-		formattedContractFund = fund
+	contentLines := make([]string, 0)
+
+	contentLines = append(contentLines, icyAddressContent(accounting)...)
+	contentLines = append(contentLines, "")
+	contentLines = append(contentLines, conversionRateContent(accounting)...)
+	contentLines = append(contentLines, "")
+	contentLines = append(contentLines, contractFundContent(accounting)...)
+	contentLines = append(contentLines, "")
+	contentLines = append(contentLines, yourEarnedICYContent(totalEarned)...)
+	contentLines = append(contentLines, "")
+	contentLines = append(contentLines, last5TxnsContent(earnedTxns)...)
+
+	msg := &discordgo.MessageEmbed{
+		Title:       fmt.Sprintf("%s Your ICY Discovery", discordEmojiPepeWhale),
+		Description: strings.Join(contentLines, "\n"),
 	}
 
-	var formattedTotalEarnedICY string = "NaN"
-	if totalEarnedICY := cryptoutils.StringBigIntToStringCurrency(totalEarned.TotalEarnsICY, 0); totalEarnedICY != "" {
-		formattedTotalEarnedICY = totalEarnedICY
+	return base.SendEmbededMessage(e.ses, original, msg)
+}
+
+func icyAddressContent(accounting *model.IcyAccounting) (result []string) {
+	result = append(result, fmt.Sprintf("%s **ICY Address**", discordEmojiPepeStonk))
+
+	if accounting != nil && accounting.ICY != nil {
+		result = append(result, fmt.Sprintf("`%s|%s`", strings.ToUpper(accounting.ICY.Chain), accounting.ICY.Address))
+	} else {
+		result = append(result, "Not found")
 	}
 
-	var formattedTotalEarnedUSDT string = "NaN"
-	if totalEarnedUSDT := cryptoutils.StringBigIntToStringCurrency(big.NewFloat(totalEarned.TotalEarnsUSD).String(), 0); totalEarnedUSDT != "" {
-		formattedTotalEarnedUSDT = totalEarnedUSDT
+	return
+}
+
+func conversionRateContent(accounting *model.IcyAccounting) (result []string) {
+	result = append(result, fmt.Sprintf("%s **Conversion Rate**", discordEmojiPepeAhegao))
+
+	if accounting != nil {
+		result = append(result, fmt.Sprintf("%s`1 ICY` ~ %s`%.1f USDT`", discordEmojiIcyToken, discordEmojiDollar, accounting.ConversionRate))
+	} else {
+		result = append(result, "Not found")
+	}
+
+	return
+}
+
+func contractFundContent(accounting *model.IcyAccounting) (result []string) {
+	result = append(result, fmt.Sprintf("%s **Contract Fund**", discordEmojiPepeMoney))
+
+	if accounting != nil && accounting.USDT != nil {
+		if fund := cryptoutils.StringBigIntToStringCurrency(accounting.ContractFundInUSDT, accounting.USDT.Decimals); fund != "" {
+			result = append(result, fmt.Sprintf("%s`%s USDT`", discordEmojiDollar, fund))
+		}
+	}
+
+	if len(result) == 1 {
+		result = append(result, "Not found")
+	}
+
+	return
+}
+
+func yourEarnedICYContent(totalEarned *model.ICYTotalEarned) (result []string) {
+	result = append(result, fmt.Sprintf("%s **Your Earned ICY**", discordEmojiPepeCoolNerd))
+
+	var (
+		formattedTotalEarnedICY  string = "NaN"
+		formattedTotalEarnedUSDT string = "NaN"
+	)
+	if totalEarned != nil {
+		if totalEarnedICY := cryptoutils.StringBigIntToStringCurrency(totalEarned.TotalEarnsICY, 0); totalEarnedICY != "" {
+			formattedTotalEarnedICY = totalEarnedICY
+		}
+		if totalEarnedUSDT := cryptoutils.StringBigIntToStringCurrency(big.NewFloat(totalEarned.TotalEarnsUSD).String(), 0); totalEarnedUSDT != "" {
+			formattedTotalEarnedUSDT = totalEarnedUSDT
+		}
+	}
+
+	result = append(result, fmt.Sprintf("%s`%s ICY` ~ %s`%s USDT`", discordEmojiIcyToken, formattedTotalEarnedICY, discordEmojiDollar, formattedTotalEarnedUSDT))
+	return
+}
+
+func last5TxnsContent(earnedTxns []*model.ICYEarnedTransaction) (result []string) {
+	result = append(result, fmt.Sprintf("%s **Your Last 5 Earns**", discordEmojiAnxinICY))
+
+	if len(earnedTxns) == 0 {
+		result = append(result, "No transaction found!")
+		return
 	}
 
 	last5Txns := [][]string{}
@@ -59,34 +134,6 @@ func (e *Icy) PersonalInfo(original *model.DiscordMessage, accounting *model.Icy
 		})
 	}
 
-	txnsTable := formatTable(last5Txns, last5Msgs)
-
-	contentLines := []string{
-		// ICY Address
-		fmt.Sprintf("%s **ICY Address**", discordEmojiPepeStonk),
-		fmt.Sprintf("`%s|%s`", strings.ToUpper(accounting.ICY.Chain), accounting.ICY.Address),
-		"",
-		// ICY conversion rate
-		fmt.Sprintf("%s **Conversion Rate**", discordEmojiPepeAhegao),
-		fmt.Sprintf("%s`1 ICY` ~ %s`%.1f USDT`", discordEmojiIcyToken, discordEmojiDollar, accounting.ConversionRate),
-		"",
-		// Contract Fund
-		fmt.Sprintf("%s **Contract Fund**", discordEmojiPepeMoney),
-		fmt.Sprintf("%s`%s USDT`", discordEmojiDollar, formattedContractFund),
-		"",
-		// Your Earned ICY
-		fmt.Sprintf("%s **Your Earned ICY**", discordEmojiPepeCoolNerd),
-		fmt.Sprintf("%s`%s ICY` ~ %s`%s USDT`", discordEmojiIcyToken, formattedTotalEarnedICY, discordEmojiDollar, formattedTotalEarnedUSDT),
-		"",
-		// Your Last 5 Transactions
-		fmt.Sprintf("%s **Your Last 5 Earns**", discordEmojiAnxinICY),
-		txnsTable,
-	}
-
-	msg := &discordgo.MessageEmbed{
-		Title:       fmt.Sprintf("%s Your ICY Discovery", discordEmojiPepeWhale),
-		Description: strings.Join(contentLines, "\n"),
-	}
-
-	return base.SendEmbededMessage(e.ses, original, msg)
+	result = append(result, formatTable(last5Txns, last5Msgs))
+	return
 }
